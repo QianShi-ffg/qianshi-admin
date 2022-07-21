f72936e54d07d3033488b0aaadc76cf3a395eea2
xingyijun <2687346406@qq.com>
2022-07-21 22:28:48 +0800

    保存功能外部调用成功

--- src/views/admin/creative/modules/articleOption.vue
+++ src/views/admin/creative/modules/articleOption.vue
@@ -6,11 +6,11 @@
       <el-button type="success">发布</el-button>
     </div>
   </header>
-  <md-editor v-model="text" @onHtmlChanged="saveHtml"  @onSave="codeSave" :onUploadImg="uploadImg"/>
+  <md-editor v-model="text" @onHtmlChanged="saveHtml"  @onSave="codeSave" :onUploadImg="uploadImg" :theme="theme"/>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted  } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import { ArrowLeft } from '@element-plus/icons-vue'
@@ -21,6 +21,8 @@ import api from '@/api/index.js'
 
 const router = useRouter()
 const text = ref('')
+const theme = ref('light')
+
 let saveIcon = null
 if(sessionStorage.getItem('md')){
   text.value = sessionStorage.getItem('md')
@@ -29,21 +31,21 @@ if(sessionStorage.getItem('md')){
 const saveHtml = (e) => {
   console.log(e)
 }
+
+const save = () => {
+  console.dir(saveIcon)
+  saveIcon[0].click()
+}
+
 onMounted(() => {
   const icon = document.getElementsByClassName('md-toolbar-item')
   saveIcon = Array.from(icon).filter(item => {
     return item.title === '保存'
   })
-  
 })
 
-const save = () => {
-  console.dir(saveIcon)
-  saveIcon.click()
-}
 
 const codeSave = (e) => {
-  console.log(e)
   sessionStorage.setItem('md', e )
   ElMessage({ message: '已保存草稿箱',type: 'success' })
 }
@@ -54,13 +56,6 @@ const uploadImg = async(files, callback) => {
     fromData.append('file', files[i]);
   }
   const res = await api.uploadImg(fromData)
-  console.log(res)
-  // files.map(item => {
-  //   console.log(item)
-    
-  //   fromData.append(item.name, item)
-  //   api.uploadImg(fromData)
-  // })
   console.log(import.meta.env.VITE_SERVER)
   callback(res.data.map((item) => `${import.meta.env.VITE_SERVER}${item.path}`))
 }
