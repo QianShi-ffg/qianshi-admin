5e4c1c0de97dc891eb1b679f7a65c37009c294cf
xingyijun <xing_yijun@venusgroup.com.cn>
2022-09-19 16:43:06 +0800

    1

--- web/vite.config.js
+++ web/vite.config.js
@@ -20,7 +20,6 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        // target: 'http://188.131.164.41:3000',
         rewrite: (path) => path.replace(/^\/api/, ''),
         changeOrigin: true
       }
