3fd0ab2012cc91b8e3a9e802b06012e7be468470
xingyijun <xing_yijun@venusgroup.com.cn>
2022-10-24 16:25:54 +0800

    连接数据库

--- nestServer/src/app.module.ts
+++ nestServer/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './modules/article/article.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ArticleModule],
+  imports: [TypeOrmModule.forRoot(), ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
