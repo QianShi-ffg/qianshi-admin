a93657bb20d507d36f00a5396e75c5a6d2119d1b
xingyijun <xing_yijun@venusgroup.com.cn>
2022-11-29 17:57:08 +0800

    开启跨域

--- nestServer/src/main.ts
+++ nestServer/src/main.ts
@@ -4,6 +4,7 @@ import { NotFoundFilter } from './not-found/not-found.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new NotFoundFilter());
   await app.listen(3022);
   console.log('3022服务已启动');
