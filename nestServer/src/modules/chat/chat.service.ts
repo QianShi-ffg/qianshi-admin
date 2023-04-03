a94b90cca2bd687e52ef7ef0dacee783e5f39210
qianshi <2687346406@qq.com>
2023-04-03 21:56:56 +0800

    111

--- nestServer/src/modules/chat/chat.service.ts
+++ nestServer/src/modules/chat/chat.service.ts
@@ -53,65 +53,76 @@ export class ChatService {
    * @returns 返回值
    */
   async chat(message: string) {
-    const { ChatGPTAPI } = await importDynamic('chatgpt');
+    // const { ChatGPTAPI } = await importDynamic('chatgpt');
     const res = await this.findOne(1);
-    clearInterval(countTimeer);
-    countTimeer = setInterval(() => {
-      count++;
-      // 10分钟没有再次访问视为不再继续使用，计时器停止计时
-      if (count >= 600) {
-        clearInterval(countTimeer);
-        count = 0;
-      }
-    }, 1000);
-    if (count === 0) {
-      // 初始访问 init
-      api = new ChatGPTAPI({ apiKey: res.key });
-    } else if (count > 0 && count <= 300) {
-      // 5分钟间隔判断在话题时间内
-      count = 1;
-    } else {
-      // 超出话题间隔时间，则重新计数，重新开始话题
-      api = new ChatGPTAPI({ apiKey: res.key });
-      count = 1;
-    }
-    const response = await this.generateResponse(api, message);
-
-    return {
-      code: 200,
-      data: [
-        {
-          text: response,
-        },
-      ],
-      message: '获取成功',
-    };
-    // const openai = new OpenAIApi(configuration);
-    // try {
-    //   const completion = await openai.createCompletion({
-    //     prompt: message,
-    //     model: 'text-davinci-003',
-    //     temperature: 0.8,
-    //     max_tokens: 1024,
-    //     top_p: 1,
-    //     frequency_penalty: 0.0,
-    //     presence_penalty: 0.6,
-    //     stop: ['Human:', 'AI:'],
-    //   });
-    //   console.log(completion.data.choices[0].text);
-    //   return {
-    //     code: 200,
-    //     data: completion.data.choices,
-    //     message: '获取数据成功',
-    //   };
-    // } catch (error) {
-    //   if (error.response) {
-    //     console.log(error.response.status);
-    //     console.log(error.response.data);
-    //   } else {
-    //     console.log(error.message);
+    // clearInterval(countTimeer);
+    // countTimeer = setInterval(() => {
+    //   count++;
+    //   // 10分钟没有再次访问视为不再继续使用，计时器停止计时
+    //   if (count >= 600) {
+    //     clearInterval(countTimeer);
+    //     count = 0;
     //   }
+    // }, 1000);
+    // if (count === 0) {
+    //   // 初始访问 init
+    //   api = new ChatGPTAPI({ apiKey: res.key });
+    // } else if (count > 0 && count <= 300) {
+    //   // 5分钟间隔判断在话题时间内
+    //   count = 1;
+    // } else {
+    //   // 超出话题间隔时间，则重新计数，重新开始话题
+    //   api = new ChatGPTAPI({ apiKey: res.key });
+    //   count = 1;
     // }
+    // const response = await this.generateResponse(api, message);
+    // return {
+    //   code: 200,
+    //   data: [
+    //     {
+    //       text: response,
+    //     },
+    //   ],
+    //   message: '获取成功',
+    // };
+    const openai = new OpenAIApi(
+      new Configuration({
+        apiKey: res.key,
+      }),
+    );
+    try {
+      const completion = await openai.createChatCompletion(
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {
+              role: 'user',
+              content: '66666',
+            },
+          ],
+        },
+        // {
+        //   proxy: {
+        //     protocol: 'proxy-protocol',
+        //     host: 'proxy-host',
+        //     port: proxy - port,
+        //   },
+        // },
+      );
+      console.log(completion.data.choices);
+      return {
+        code: 200,
+        data: completion.data.choices,
+        message: '获取数据成功',
+      };
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
     // const response = await openai11.listEngines();
   }
 
